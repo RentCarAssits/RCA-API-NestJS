2f9a3f436724d5c69b9714230ef16c79973af9c3
Apolosmllb <minermllb@gmail.com>
2023-05-25 00:06:31 -0500

    Initial project setup, modular monolith andd DDD folder structure

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { RentingManagementModule } from './renting-management/renting-management.module';
+import { BillingManagementModule } from './billing-management/billing-management.module';
+import { SubscriptionManagementModule } from './subscription-management/subscription-management.module';
+import { WorkshopServiceManagementModule } from './workshop-service-management/workshop-service-management.module';
+
+@Module({
+  imports: [
+    RentingManagementModule,
+    BillingManagementModule,
+    SubscriptionManagementModule,
+    WorkshopServiceManagementModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}

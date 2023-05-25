b19a9b0c25ca22a08cc697aaa4394959ac62dce7
XFernando43 <102422211+XFernando43@users.noreply.github.com>
2023-05-25 18:52:35 -0500

    Add: new AccountID value
    I Add AccountId Value Object

---
+++ src/subscription-management/domain/values/AccountEmail.ts
@@ -0,0 +1,18 @@
+import { PrimaryColumn } from "typeorm";
+
+export class AccountId {
+  @PrimaryColumn('int', { name: 'id' })
+  protected readonly value: number;
+
+  protected constructor(value: number) {
+    this.value = Number(value);
+  }
+
+  public static of(value: number): AccountId {
+    return new AccountId(value);
+  }
+
+  public getValue(): number {
+    return Number(this.value);
+  }
+}

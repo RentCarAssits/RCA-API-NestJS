3caa1e6e57735f24a7668b24d85d825db679992e
Apolosmllb <54141767+Apolosmllb@users.noreply.github.com>
2023-05-28 20:55:50 -0500

    add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as config from 'dotenv';
+import * as configv1 from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  config.config();
+  configv1.config();
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('RCA-API')
+    .setDescription('Rent Car Assists Api Documentation')
+    .setVersion('1.0')
+    .addTag('CRA')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.enableCors({ origin: '*' });
-  const port = (process.env.PORT || process.env.LOCAL_PORT)?? 3000;
+  const port = (process.env.PORT || process.env.LOCAL_PORT) ?? 3000;
   await app.listen(port);
 }
+
 bootstrap();

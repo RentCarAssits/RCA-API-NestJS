8f1ea9affc5bb51c715a3ea604684a66ca7855c9
XFernando43 <102422211+XFernando43@users.noreply.github.com>
2023-05-29 04:47:20 -0500

    Add: Entity
    me falta ver el tema de la relación con los planes.

--- src/subscription-management/domain/entity/plan.entity.ts
+++ src/subscription-management/domain/entity/plan.entity.ts
@@ -1,5 +1,41 @@
 import { AggregateRoot } from "@nestjs/cqrs";
+import { ApiProperty } from "@nestjs/swagger";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { PlanId } from "../values/plan-id.value";
+import { PlanName } from "../values/plan-name.value";
+import { Benefits } from "../values/benefits.value";
 
-export class Plan extends AggregateRoot{
-    
+@Entity('Plans')
+export class Plan extends AggregateRoot {
+  @ApiProperty()
+  @PrimaryGeneratedColumn()
+  private id: PlanId;
+
+  @ApiProperty()
+  @Column(() => PlanName, { prefix: false })
+  private readonly PlanName: PlanName;
+
+  /*
+  @ApiProperty()
+  @OneToMany(()=> Benefits, (Benefits)=> Benefits.getValue,{
+    cascade:true,
+    eager:true,
+  })  
+  benefits?: Benefits[];
+*/ 
+  // aun no tengo claro como relacionarlo
+
+  public constructor(PlanName: PlanName) {
+    super();
+    this.PlanName = PlanName;
+  }
+
+  public getId(): PlanId {
+    return this.id;
+  }
+
+  public getPlanName(): PlanName {
+    return this.PlanName;
+  }
+
 }

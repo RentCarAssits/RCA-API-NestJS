61639c35199e6877b3fd75e0d7bcc3b0ffd46660
Apolosmllb <minermllb@gmail.com>
2023-05-30 17:30:36 -0500

    add user and account entity

---
+++ src/iam-management/domain/entities/account.entity.ts
@@ -0,0 +1,30 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToOne } from 'typeorm';
+import { User } from './user.entity';
+
+@Entity('accounts')
+export class Account {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('text')
+  username: string;
+
+  @Column('text')
+  email: string;
+
+  @Column('text', {
+    select: false,
+  })
+  password: string;
+
+  @OneToOne(() => User, (usuario) => usuario.account)
+  usuario: User;
+
+  @Column('simple-array')
+  roles: string[];
+
+  //
+
+}
+
+//AÑADIR REALACIONES PARA LOS DEMÁS BOUNDED CONTEXT

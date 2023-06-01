321f45d883c685fa3ea40131df676dbf19669d7e
Sebastian Montes <u201712366@upc.edu.pe>
2023-06-01 17:48:13 -0500

    feat: Account Payable - request - response

---
+++ src/billing-management/application/reponses/register-accountPayable.response.ts
@@ -0,0 +1,9 @@
+export class RegisterAccountPayableResponse {
+    constructor(
+      public readonly accountPayableId: number,
+      public readonly payerId: number,
+      public readonly payeeId: number,
+      public readonly totalPrice: number,
+      public readonly expirationDay: Date
+    ) {}
+  }

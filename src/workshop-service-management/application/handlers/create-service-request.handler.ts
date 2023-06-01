55daa0603653e07829ae33e99c0ebf5d0d3918ed
Coltmoons <coltmoonsac@gmail.com>
2023-06-01 18:44:56 -0500

    feat- create service request handler

---
+++ src/workshop-service-management/application/handlers/create-service-request.handler.ts
@@ -0,0 +1,47 @@
+import { CommandHandler, EventPublisher, ICommandHandler } from '@nestjs/cqrs';
+import { CreateServiceRequest } from '../commands/create-service-request.command';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ServiceRequest } from 'src/workshop-service-management/domain/entities/service-request.entity';
+import { Repository } from 'typeorm';
+import { WorkshopIdFK } from 'src/workshop-service-management/domain/value-objects/workshop-id-fk.value';
+import { OwnerIdFK } from 'src/workshop-service-management/domain/value-objects/owner-id-fk.value';
+import { VehicleIdFK } from 'src/workshop-service-management/domain/value-objects/vehicle-id-fk.value';
+import { ServiceRequestFactory } from 'src/workshop-service-management/domain/factory/service-request-factory.factory';
+
+@CommandHandler(CreateServiceRequest)
+export class CreateServiceRequestHandler
+  implements ICommandHandler<CreateServiceRequest>
+{
+  constructor(
+    @InjectRepository(ServiceRequest)
+    private serviceRequestRepository: Repository<ServiceRequest>,
+    private publisher: EventPublisher,
+  ) {}
+
+  async execute(command: CreateServiceRequest): Promise<any> {
+    let serviceRequestId = 0;
+    const descriptionProblems: string = command.descriptionProblems;
+    const workshopId: WorkshopIdFK = WorkshopIdFK.of(command.workshopId);
+    const ownerId: OwnerIdFK = OwnerIdFK.of(command.ownerId);
+    const vehicleId: VehicleIdFK = VehicleIdFK.of(command.vehicleId);
+
+    const serviceRequestEntity: ServiceRequest =
+      ServiceRequestFactory.createFrom(
+        descriptionProblems,
+        workshopId,
+        vehicleId,
+        ownerId,
+      );
+
+    let serviceRequest = await this.serviceRequestRepository.save(
+      serviceRequestEntity,
+    );
+
+    if(serviceRequest === null) {
+      return serviceRequestId;
+    }
+
+    serviceRequestId = Number(serviceRequest.getId());
+    
+  }
+}

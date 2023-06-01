822813f610029d34be112ffa47e4d107c2855635
Sebastian Montes <u201712366@upc.edu.pe>
2023-06-01 17:50:21 -0500

    fix: Configuration Billing Management for Account Payable Aggregate

--- src/billing-management/billing-management.module.ts
+++ src/billing-management/billing-management.module.ts
@@ -1,4 +1,27 @@
 import { Module } from '@nestjs/common';
+import { AccountPayableController } from './api/account-payable.controller';
+import { CreateAccountPayable } from './application/commands/register-accountPayable.command';
+import { CreateAccountPayableHandler } from './application/handlers/register-accountPayable.handlre';
+import { CqrsModule } from '@nestjs/cqrs';
+import { AccountPayableApplicationService } from './application/services/accountPayable-application.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PaymentPayableAggregate } from './domain/aggregates/payment-payable';
+import { AccountPayableAggregate } from './domain/aggregates/account-payable';
 
-@Module({})
+export const CommandHandlers=[CreateAccountPayableHandler];
+
+@Module({
+    imports:[
+        TypeOrmModule.forFeature([PaymentPayableAggregate,AccountPayableAggregate]),
+        CqrsModule,
+        BillingManagementModule
+    ],
+    controllers:[
+        AccountPayableController
+    ],
+    providers:[
+        AccountPayableApplicationService, 
+        ...CommandHandlers
+    ]
+})
 export class BillingManagementModule {}

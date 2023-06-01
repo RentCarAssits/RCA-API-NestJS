eda8fb0bb94b25565fbb7200f0f65a2b7fcb3606
Sebastian Trujillo <u201914877@upc.edu.pe>
2023-06-01 13:20:04 -0500

    Make some changes in payment_payable

--- src/billing-management/domain/aggregates/payment-payable.ts
+++ src/billing-management/domain/aggregates/payment-payable.ts
@@ -1,47 +1,45 @@
 import { AggregateRoot } from '@nestjs/cqrs';
-import { Column, Entity, PrimaryColumn, ValueTransformer } from 'typeorm';
-import { PaymentId } from '../values/Payment-id.value';
-import { AccountPayableId } from '../values/account-payable-id.value';
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn, ValueTransformer } from 'typeorm';
+import { PaymentIdFk } from '../values/payment-id-fk.value';
+import { AccountPaybleIdFk } from '../values/account-payable-id-fk.value';
 import { Amount } from '../values/amount.value';
 
-@Entity()
+@Entity('payment_payable')
 export class PaymentPayableAggregate extends AggregateRoot {
-  @PrimaryColumn('bigint', { name: 'id' })
-  private paymentId: PaymentId;
+  @PrimaryColumn('bigint', { name: 'payment_id' })
+  private paymentId: PaymentIdFk;
 
-  @Column({
-    type: 'bigint',
-    name: 'account_payable_id',
-    transformer: {
-      to: (value: AccountPayableId) => value.getValue(),
-      from: (value: number) => AccountPayableId.create(value),
-    },
-  })
-  private accountPayableId: AccountPayableId;
+  @PrimaryColumn('bigint', { name: 'account_payable_id' })
+  private accountPayableId: AccountPaybleIdFk;
 
   @Column((type) => Amount)
   private amount: Amount;
 
-  constructor(
-    idValue: number,
-    accountPayableIdValue: number,
-    amountValue: number
-  ) {
+  public constructor(paymentId: PaymentIdFk,
+    accountPayableId: AccountPaybleIdFk,
+    amountValue: Amount) { //Cuando creas el paymentPayable, le pasas la id del pago, la id de la deuda que esta pagando y el monto que esta pagando
     super();
-    this.paymentId = PaymentId.create(idValue);
-    this.accountPayableId = AccountPayableId.create(accountPayableIdValue);
-    this.amount = Amount.create(amountValue);
+    this.paymentId = paymentId;
+    this.accountPayableId = accountPayableId;
+    this.amount = amountValue;
   }
 
-  public getPaymentId(): PaymentId {
+  public getPaymentId(): PaymentIdFk {
     return this.paymentId;
   }
 
-  public getAccountPayableId(): AccountPayableId {
+  public getAccountPayableId(): AccountPaybleIdFk {
     return this.accountPayableId;
   }
 
   public getAmount(): Amount {
     return this.amount;
+  }
+
+  public updateParcialPrice(){ //PARCIALPRICE=TOTALPRICE-AMOUNT
+    //const accountPayable = await this.accountPayableRepository.findOne({ where: { id: this.accountPayableId } }); 
+    //TotalPrice= accountPayable.findTotalPricebyAccountPayableId()
+    //ParcialPrice=TotalPrice-this.amount
+    //updateParcialPrice(ParcialPrice)
   }
 }

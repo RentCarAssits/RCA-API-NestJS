b8038f011413bff2ab70146a1367dc322c4f66a5
Coltmoons <coltmoonsac@gmail.com>
2023-06-01 17:53:44 -0500

    add service request dto

---
+++ src/workshop-service-management/application/dtos/service-request.dto.ts
@@ -0,0 +1,7 @@
+export class ServiceRequestDto {
+  public id: number;
+  public readonly descriptionProblems: string;
+  public readonly workshopId: number;
+  public readonly ownerId: number;
+  public readonly vehicleId: number;
+}

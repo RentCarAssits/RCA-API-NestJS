2bc3a9d384bc98b2f70f7fdc9a60de81201deb9a
XFernando43 <102422211+XFernando43@users.noreply.github.com>
2023-06-05 00:50:49 -0500

    fix: fix-subscriptions-services.

--- src/subscription-management/application/dtos/SubscriptionDto.ts
+++ src/subscription-management/application/dtos/SubscriptionDto.ts
@@ -1,6 +1,6 @@
 export class SubscriptionDto {
   public PlanId: number;
-  public AccountId: number;
+  //public AccountId: number;
   public UnitPrice: number;
   public Frequency: string;
   public startDate: Date;
--- src/subscription-management/application/service/subscription-application.service.ts
+++ src/subscription-management/application/service/subscription-application.service.ts
@@ -11,42 +11,36 @@ import { RegisterSubscriptionResponse } from "../response/register-subscription.
 
 @Injectable()
 export class subscriptionApplicationService{
-    constructor(private connection: Connection,private commandBus: CommandBus,
-        private registerSubscriptionValidator: RegisterSubscriptionValidator,
-      ) {
-        console.log('this.connection.isConnected: ', this.connection.isConnected);
-      }
+  constructor(private connection: Connection,private commandBus: CommandBus,
+    private registerSubscriptionValidator: RegisterSubscriptionValidator,
+  ) { console.log('this.connection.isConnected: ', this.connection.isConnected);}
 
-      async register(
-        RegisterSubscriptionRequest: RegisterSubscriptionRequest,
-      ): Promise<Result<AppNotification,RegisterSubscriptionResponse>>{
-        
-        const notification: AppNotification = await this.registerSubscriptionValidator.validate(RegisterSubscriptionRequest);
-        if(notification.hasErrors()){return Result.error(notification);}
-       
-        const registerSubscription: RegisterSubscription = new RegisterSubscription(
-            //RegisterSubscriptionRequest.AccountId,
-            RegisterSubscriptionRequest.PlanId,
-            RegisterSubscriptionRequest.UnitPrice,
-            RegisterSubscriptionRequest.Frequency,
-            RegisterSubscriptionRequest.startDate,
-            RegisterSubscriptionRequest.endDate,   
-            //RegisterSubscriptionRequest.PeriodId2.
-          );  
-        
-          const subscriptionId: number = await this.commandBus.execute(registerSubscription);
-          
-          const registerSubscriptionResponse: RegisterSubscriptionResponse =
-          new RegisterSubscriptionResponse(
-            subscriptionId,
-            registerSubscription.planId,
-            //registerSubscription.AccountId,
-            registerSubscription.unitPrice,
-            registerSubscription.frequency,
-            registerSubscription.startDate,
-            registerSubscription.endDate,
-          );
-        return Result.ok(registerSubscriptionResponse);
-      }
-
+  async register(RegisterSubscriptionRequest: RegisterSubscriptionRequest,): 
+    Promise<Result<AppNotification,RegisterSubscriptionResponse>>{
+    
+      const notification: AppNotification = await this.registerSubscriptionValidator.validate(RegisterSubscriptionRequest);
+      if(notification.hasErrors()){return Result.error(notification);} 
+      
+      const registerSubscription: RegisterSubscription = new RegisterSubscription(
+      //RegisterSubscriptionRequest.AccountId,
+      RegisterSubscriptionRequest.PlanId,
+      RegisterSubscriptionRequest.UnitPrice,
+      RegisterSubscriptionRequest.Frequency,
+      RegisterSubscriptionRequest.startDate,
+      RegisterSubscriptionRequest.endDate,   
+      );      
+    
+    const subscriptionId: number = await this.commandBus.execute(registerSubscription);     
+      const registerSubscriptionResponse: RegisterSubscriptionResponse =
+      new RegisterSubscriptionResponse(
+        subscriptionId,
+        registerSubscription.planId,
+      //registerSubscription.AccountId,
+        registerSubscription.unitPrice,
+        registerSubscription.frequency,
+        registerSubscription.startDate,
+        registerSubscription.endDate,
+        );
+      return Result.ok(registerSubscriptionResponse);
+    }
 }

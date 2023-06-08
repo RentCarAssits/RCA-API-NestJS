d868cca611b12e3d3268264b00cfa4254d67859e
Coltmoons <coltmoonsac@gmail.com>
2023-06-07 23:35:27 -0500

    create service request service

--- src/workshop-service-management/application/dtos/service-request-response.dto.ts
+++ src/workshop-service-management/application/dtos/service-request-response.dto.ts
@@ -1,5 +1,6 @@
-export class ServiceRequestDto {
+export class ServiceRequestResponseDto {
   constructor(
+    public readonly id: number,
     public readonly descriptionProblems: string,
     public readonly workshopId: number,
     public readonly ownerId: number,

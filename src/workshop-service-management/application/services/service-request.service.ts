3653065ce11d97bd46d7e3141b054c0acde40fe8
Coltmoons <coltmoonsac@gmail.com>
2023-06-07 20:13:08 -0500

    create validator

---
+++ src/workshop-service-management/application/services/service-request.service.ts
@@ -0,0 +1,10 @@
+// import { Injectable } from "@nestjs/common";
+// import { CommandBus } from "@nestjs/cqrs";
+
+// @Injectable()
+// export class ServiceRequestService {
+//   constructor(
+//     private commandBus: CommandBus,
+//     private
+//   )
+// }
---
+++ src/workshop-service-management/application/validators/create-service-request.validator.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ServiceRequest } from 'src/workshop-service-management/domain/entities/service-request.entity';
+import { Repository } from 'typeorm';
+import { ServiceRequestDto } from '../dtos/service-request.dto';
+import { AppNotification } from 'src/shared/application/app.notification';
+
+@Injectable()
+export class CreateServiceRequestValidator {
+  constructor(
+    @InjectRepository(ServiceRequest)
+    private serviceRequestRepository: Repository<ServiceRequest>,
+  ) {}
+
+  public async validate(
+    serviceRequestDto: ServiceRequestDto,
+  ): Promise<AppNotification> {
+    const notification: AppNotification = new AppNotification();
+    const descriptionProblems: string = serviceRequestDto.descriptionProblems;
+    if (descriptionProblems === null) {
+      notification.addError('Description Problem is required', null);
+    }
+
+    const workshopId: number = serviceRequestDto.workshopId;
+    if (workshopId == null) {
+      notification.addError('Workshop Id is required', null);
+    }
+
+    const vehicleId: number = serviceRequestDto.vehicleId;
+    if (vehicleId == null) {
+      notification.addError('Vehicle id is required', null);
+    }
+
+    const ownerId: number = serviceRequestDto.ownerId;
+    if (ownerId == null) {
+      notification.addError('Owner id is required', null);
+    }
+
+    return notification;
+  }
+}

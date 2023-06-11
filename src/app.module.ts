98f5eb5b4627bae0fda02029c86a28d2c4854825
Miner Lozano <54141767+Apolosmllb@users.noreply.github.com>
2023-06-11 14:21:03 -0500

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,14 +9,33 @@ import { SharedModule } from './shared/shared.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { IamManagementModule } from './iam-management/iam-management.module';
-import * as ormconfig from './../../RCA-API-NestJS/ormconfig.js';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
 
-    TypeOrmModule.forRoot(ormconfig),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'rca-parsimonia-server.mysql.database.azure.com',
+      port: 3306,
+      username: 'rca123parsimonia',
+      password: 'edco-password1',
+      database: 'rca-db',
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: true,
+      //migrationsRun: true,
+      logging: false,
+      //timezone: '+0',
+      bigNumberStrings: false,
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+    }),
     RentingManagementModule,
     BillingManagementModule,
     SubscriptionManagementModule,

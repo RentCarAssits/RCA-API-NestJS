036b309ed9131de8e2547c5b4bce899c8c879173
Miner Lozano <minermllb@gmail.com>
2023-06-14 17:09:22 -0500

    Update iam-management.module.ts

--- src/iam-management/iam-management.module.ts
+++ src/iam-management/iam-management.module.ts
@@ -1,16 +1,16 @@
-import { Module } from '@nestjs/common';
-import { UsersController } from './api/users/users.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from "@nestjs/common";
+import { UsersController } from "./api/users/users.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
-import { AuthController } from './api/auth/auth.controller';
-import { AuthService } from './application/services/auth/auth.service';
-import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtStrategy } from './infrastructure/strategies/jwt.strategy';
-import { User } from './domain/entities/user.entity';
-import { Account } from './domain/entities/account.entity';
-import { Profile } from './domain/entities/profile.entity';
+import { AuthController } from "./api/auth/auth.controller";
+import { AuthService } from "./application/services/auth/auth.service";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { JwtStrategy } from "./infrastructure/strategies/jwt.strategy";
+import { User } from "./domain/entities/user.entity";
+import { Account } from "./domain/entities/account.entity";
+import { Profile } from "./domain/entities/profile.entity";
 
 @Module({
   controllers: [UsersController, AuthController],
@@ -18,19 +18,20 @@ import { Profile } from './domain/entities/profile.entity';
   imports: [
     ConfigModule,
     TypeOrmModule.forFeature([User, Account, Profile]),
-    PassportModule.register({ defaultStrategy: 'jwt' }),
+    PassportModule.register({ defaultStrategy: "jwt" }),
     JwtModule.registerAsync({
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => {
         return {
-          secret: 'sflkahfkjhakjfhakjsfhsakjfhk',
+          secret: configService.get("JWT_SECRET"),
           signOptions: {
-            expiresIn: '12h',
-          },
+            expiresIn: "12h"
+          }
         };
-      },
-    }),
+      }
+    })
   ],
-  exports: [TypeOrmModule, JwtStrategy, PassportModule, JwtModule],
+  exports: [TypeOrmModule, JwtStrategy, PassportModule, JwtModule]
 })
-export class IamManagementModule {}
+export class IamManagementModule {
+}

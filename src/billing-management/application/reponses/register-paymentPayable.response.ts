3556bd6a17586b283c096dce0f595beebed984f2
Sebastian Trujillo <u201914877@upc.edu.pe>
2023-06-21 13:25:42 -0500

    feat: Correction of payment payable response

--- src/billing-management/application/reponses/register-paymentPayable.response.ts
+++ src/billing-management/application/reponses/register-paymentPayable.response.ts
@@ -1,6 +1,6 @@
 export class RegisterPaymentPayableResponse{
     constructor(
-        public readonly paymentPayableId:number,
+        public readonly id:number,
         public readonly paymentId:number,
         public readonly accountPayableId:number,
         public readonly amount:number

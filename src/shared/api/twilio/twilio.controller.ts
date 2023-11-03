81f2aa39bf3342d002291befc7a14a9fc2f11244
Miner Lozano <54141767+Apolosmllb@users.noreply.github.com>
2023-11-03 12:27:05 -0500

    Update twilio.controller.ts

--- src/shared/api/twilio/twilio.controller.ts
+++ src/shared/api/twilio/twilio.controller.ts
@@ -6,11 +6,6 @@ export class TwilioController {
   constructor(private messageFacade: TwilioFacade) {}
   @Post()
   async generate(@Body() request: any) {
-    console.log(
-      '🚀 ~ file: twilio.controller.ts:10 ~ TwilioController ~ generate ~ request:',
-      request,
-    );
-
     try {
       const message = await this.messageFacade.sendWsp(request);
       return { message };
